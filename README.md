59bd949ad7fa517dee2e5684fa6bb8e90e662829
Tarou Mac <bestrrivai@gmail.com>
2024-11-18 12:47:44 +0900

    課題文を追記

--- README.md
+++ README.md
@@ -38,3 +38,55 @@ win --> end1
 if -->|no| loose
 loose --> end1
 ```
+ファイル名|説明
+-|-
+app5.js|プログラム本体
+public/number.html|数予想開始画面
+number.ejs|数予想ファイル
+
+## 仕様書
+1.```app5.js``` をnodeでポート開放行う
+1. 他のターミナルでポートにアクセスする
+1. Webブラウザでlocalhost:8080/public/number.htmlにアクセスする
+1. コンピュータが出す数を予想する
+1. 連続的中数とゲーム数が表示されるので,ゲームを続ける
+
+
+```mermaid
+flowchart TD;
+
+start["開始"];
+end1["終了"]
+if{"予想が的中しているか"}
+win["正解"]
+loose["ハズレ"]
+
+start --> if
+if -->|yes| win
+win --> end1
+if -->|no| loose
+loose --> end1
+```
+
+## 仕様書
+1.```app5.js``` をnodeでポート開放行う
+1. 他のターミナルでポートにアクセスする
+1. Webブラウザでlocalhost:8080/public/whether.htmlにアクセスする
+1. 明日の天気を予想する
+1. 連続的中数とゲーム数が表示されるので,ゲームを続ける
+
+```mermaid
+flowchart TD;
+
+start["開始"];
+end1["終了"]
+if{"予想が的中しているか"}
+win["正解"]
+loose["ハズレ"]
+
+start --> if
+if -->|yes| win
+win --> end1
+if -->|no| loose
+loose --> end1
+```
--- app5.js
+++ app5.js
@@ -29,8 +29,8 @@ app.get("/luck", (req, res) => {
 
 app.get("/janken", (req, res) => {
   let hand = req.query.hand;
-  let win = Number( req.query.win );
-  let total = Number( req.query.total );
+  let win = Number( req.query.win )||0;
+  let total = Number( req.query.total )||0;
   console.log( {hand, win, total});
   const num = Math.floor( Math.random() * 3 + 1 );
   let cpu = '';
@@ -38,9 +38,21 @@ app.get("/janken", (req, res) => {
   else if( num==2 ) cpu = 'チョキ';
   else cpu = 'パー';
   // ここに勝敗の判定を入れる
+  let judgement = '';
+  if((hand=='グー' && cpu=='チョキ')||
+  (hand=='チョキ' && cpu=='パー')||
+  (hand=='パー' && cpu=='グー')){
+    judgement ='勝ち';
+    win +=1;
+  } else if (hand === cpu) {
+  judgement = 'あいこ';
+} else {
+  judgement = '負け';
+}
+
   // 今はダミーで人間の勝ちにしておく
-  let judgement = '勝ち';
-  win += 1;
+
+
   total += 1;
   const display = {
     your: hand,
@@ -51,5 +63,64 @@ app.get("/janken", (req, res) => {
   }
   res.render( 'janken', display );
 });
+
+app.get('/number', (req,res) => {
+  let number = req.query.number;
+  let win = Number( req.query.win )||0;
+  let total = Number( req.query.total )||0;
+  console.log( {number, win, total});
+  const num = Math.floor( Math.random()* 3+1);
+let cpu = '';
+if( num==1 ) cpu = '1';
+else if( num==2 ) cpu = '2';
+else if( num==3 ) cpu = '3';
+let judgement='';
+if ((number == '3' && cpu == '3') || (number == '2' && cpu == '2')) {
+  judgement='正解';
+win +=1;}
+  else{
+    judgement = 'ハズレ';
+  }
+
+
+  total += 1;
+  const display = {
+    your: number,
+    cpu: cpu,
+    judgement: judgement,
+    win: win,
+    total: total
+  }
+  res.render( 'number', display );
+});
+
+app.get('/whether', (req,res) => {
+  let whether = req.query.whether;
+  let win = Number( req.query.win )||0;
+  let total = Number( req.query.total )||0;
+  console.log( {whether, win, total});
+  const num = Math.floor( Math.random()* 3+1);
+let cpu = '';
+if( num==1 )cpu = '晴れ';
+else if( num==2 )cpu = '曇り';
+else if( num==3 ) cpu = '雨';
+let judgement='';
+if ((whether === cpu)){
+  judgement='正解';
+win +=1;}
+  else{
+    judgement = 'ハズレ';
+  }
+  total += 1;
+  const display = {
+    your: whether,
+    cpu: cpu,
+    judgement: judgement,
+    win: win,
+    total: total
+  }
+res.render( 'whether', display );
+});
+
 
 app.listen(8080, () => console.log("Example app listening on port 8080!"));
